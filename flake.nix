e726599aba122f9fc0405d7dd9d7327b94306810
Herman Lederer <germans.lederers@gmail.com>
2023-02-21 14:51:37 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,17 +3,20 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
   };
 
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        pnpm = pkgs.nodePackages.pnpm.override {
+          nodejs = pkgs.nodejs-18_x;
+        };
       in {
         devShell = pkgs.mkShell rec {
           nativeBuildInputs = with pkgs; [
-            nodejs-18_x.pkgs.pnpm
+            pnpm
           ];
         };
       }
