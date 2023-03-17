334c4e84329cf3f401c0ac54323712b8f7086667
Herman Lederer <germans.lederers@gmail.com>
2023-03-17 19:42:38 +0100

    Sort package.json and address standartization issues

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   sourcemap: false,
   clean: true,
   dts: true,
-  format: ['esm'],
+  format: ['cjs', 'esm'],
   target: 'node14',
   outDir: 'lib',
   external: ['virtual:fs'],
