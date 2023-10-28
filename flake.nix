16c07e3b7c339cde354d54873e7042921fd54cb0
Star Lederer <germans.lederers@gmail.com>
2023-10-28 19:12:04 +0200

    Update flake and pnpm lockfile

--- flake.nix
+++ flake.nix
@@ -1,22 +1,18 @@
 {
-  description = "pnpm";
-
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
   };
 
   outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        pnpm = pkgs.nodePackages.pnpm.override {
-          nodejs = pkgs.nodejs-18_x;
-        };
       in {
-        devShell = pkgs.mkShell rec {
+        devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
-            pnpm
+            bashInteractive
+            nodePackages.pnpm
           ];
         };
       }
